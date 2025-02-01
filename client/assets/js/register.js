288db1c4635234995b79f6aa23a586affbd1a370
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-02-01 01:35:34 -0300

    primer intento validaciones register

--- client/assets/js/register.js
+++ client/assets/js/register.js
@@ -3,3 +3,10 @@ const emailInput = document.querySelector("#emailInput");
 const passwordInput = document.querySelector("#passwordInput");
 const confirmPasswordInput = document.querySelector("#passwordConfirm");
 const registerButton = document.querySelector("#registerButton");
+
+const validarDatos = () => {
+    if (nameInput.value === "" || emailInput.value === "" || passwordInput.value === "" || confirmPasswordInput.value === "") {
+        alert("Todos los campos son obligatorios");
+    }
+    return true;
+};
