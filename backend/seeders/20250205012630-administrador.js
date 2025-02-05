a29c05ef2144cde6535b07cf0f4fe4be63786ed6
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-02-04 22:38:28 -0300

    seed admin

---
+++ backend/seeders/20250205012630-administrador.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const { UUIDV4 } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('administrador', [
+      {
+        id: "65648d54-f892-48eb-9368-346a853aeaee",
+        name: 'John',
+        email: 'example@example.com',
+        password: "1234",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('administrador', null, {});
+  },
+};
