0a170b5d0db44e4b734823e4c0247980c86e721a
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-02-05 14:33:13 -0300

    seeder de alumno y notas

--- backend/models/administrador.js
+++ backend/models/administrador.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Admin extends Model {
+  class Administrador extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,13 +13,13 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Admin.init({
+  Administrador.init({
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Admin',
+    modelName: 'Administrador',
   });
-  return Admin;
+  return Administrador;
 };
