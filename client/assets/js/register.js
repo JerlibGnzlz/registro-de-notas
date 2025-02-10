bc11d5430e50adc37829e54fe6175c5818f757f1
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-02-10 00:09:47 -0300

    modularización de formularios en Register

--- client/assets/js/register.js
+++ client/assets/js/register.js
@@ -1,64 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
+import { validaciones } from "./validaciones.js";
     const form = document.querySelector("#registerForm");
     const nameInput = document.querySelector("#nameInput");
     const emailInput = document.querySelector("#emailInput");
     const passwordInput = document.querySelector("#passwordInput");
     const confirmPasswordInput = document.querySelector("#passwordConfirm");
 
-    form.addEventListener("submit", (event) => {
-        event.preventDefault(); // Evita el envío del formulario por defecto
-        console.log("Evento submit detectado");
-
-        // Validar que los campos no estén vacíos
-        if (nameInput.value.trim() === "" || 
-            emailInput.value.trim() === "" || 
-            passwordInput.value.trim() === "" || 
-            confirmPasswordInput.value.trim() === "") {
-                Swal.fire({
-                    icon: "error",
-                    title: "Campos obligatorios",
-                    text: "Todos los campos son obligatorios.",
-                });
-            return;
-        }
-
-        // Validar que las contraseñas coincidan
-        if (passwordInput.value !== confirmPasswordInput.value) {
-            Swal.fire({
-                icon: "error",
-                title: "Contraseñas no coinciden",
-                text: "Las contraseñas no coinciden.",
-            });
-            return;
-        }
-
-        // Validar longitud de la contraseña
-        if (passwordInput.value.length < 6) {
-            Swal.fire({
-                icon: "error",
-                title: "Contraseña demasiado corta",
-                text: "La contraseña debe tener al menos 6 caracteres.",
-            });
-            return;
-        }
-
-        // Validar formato del correo
-        if (!emailInput.value.includes("@") || !emailInput.value.includes(".")) {
-            Swal.fire({
-                icon: "error",
-                title: "Email inválido",
-                text: "El email debe contener una '@' y un punto.",
-            });
-            return;
-        }
-
-        // Mensaje de éxito y envío del formulario
-        Swal.fire({
-            icon: "success",
-            title: "Registro exitoso",
-            text: "Redirigiendo...",
-        }).then(() => {
-            form.submit();
-        });
-    });
-});
+   validaciones(form, nameInput, emailInput, passwordInput, confirmPasswordInput);
