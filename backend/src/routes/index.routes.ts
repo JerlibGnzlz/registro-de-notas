18c74fb398b60d9ad6059f0763cec8cb237f4abc
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-02-17 00:02:54 -0300

    creada la session y jwt

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { authRoutes } from "../administrador/auth.routes";
 import { alumnoRoutes } from "../alumno/alumno.routes";
 import { notaRoutes } from "../notas/notas.routes";
+import { authToken } from "../middlewares/AuthToken";
 
 
 export const indexRoutes = Router()
@@ -9,7 +10,7 @@ export const indexRoutes = Router()
 
 indexRoutes.use("/auth", authRoutes)
 
-indexRoutes.use("/alumno", alumnoRoutes)
+indexRoutes.use("/alumno", authToken, alumnoRoutes)
 
 indexRoutes.use("/nota", notaRoutes)
 
